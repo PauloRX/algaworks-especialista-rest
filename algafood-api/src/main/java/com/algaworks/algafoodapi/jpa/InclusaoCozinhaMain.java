5a5478e489822e5e9393fa718eef3a8eb773a7ce
pauloxavier <pauloxavier@192.168.0.175>
2022-07-15 00:13:32 -0300

    3.9. Adicionando um objeto no banco de dados
    Criado o metodo adicionar em CadastroCozinha e um novo Main para
    executar a inclusao de uma cozinha
    
    Usamos a anotacao @Transational do spring para trabalhar dentro do
    escopo de uma transação no banco de dados

---
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/jpa/InclusaoCozinhaMain.java
@@ -0,0 +1,33 @@
+package com.algaworks.algafoodapi.jpa;
+
+import java.util.List;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.algaworks.algafoodapi.AlgafoodApiApplication;
+import com.algaworks.algafoodapi.domain.model.Cozinha;
+
+public class InclusaoCozinhaMain {
+
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+		
+		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+		
+		Cozinha brasa = new Cozinha();
+		brasa.setNome("Brasileira");
+		
+		Cozinha japa = new Cozinha();
+		japa.setNome("Japonesa");
+		
+		cadastroCozinha.adicionar(brasa);
+		cadastroCozinha.adicionar(japa);
+		
+
+	}
+
+}
