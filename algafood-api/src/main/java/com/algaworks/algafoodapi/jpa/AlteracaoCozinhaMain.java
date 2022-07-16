57280ee6b5540a02b37ea22a2581f51f7bc139c4
pauloxavier <pauloxavier@192.168.0.175>
2022-07-15 21:19:08 -0300

    3.11. Atualizando um objeto no banco de dados

--- algafood-api/src/main/java/com/algaworks/algafoodapi/jpa/AlteracaoCozinhaMain.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/jpa/AlteracaoCozinhaMain.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafoodapi.jpa;
 
+import java.util.List;
+
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
@@ -7,7 +9,7 @@ import org.springframework.context.ApplicationContext;
 import com.algaworks.algafoodapi.AlgafoodApiApplication;
 import com.algaworks.algafoodapi.domain.model.Cozinha;
 
-public class InclusaoCozinhaMain {
+public class AlteracaoCozinhaMain {
 
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
@@ -16,11 +18,14 @@ public class InclusaoCozinhaMain {
 		
 		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
 		
-		Cozinha cozinha = new Cozinha();
-		cozinha.setId(1L);
-		cozinha.setNome("Brasileira");
+		Cozinha brasa = new Cozinha();
+		brasa.setNome("Brasileira");
 		
-		cadastroCozinha.adicionar(cozinha);
+		Cozinha japa = new Cozinha();
+		japa.setNome("Japonesa");
+		
+		cadastroCozinha.adicionar(brasa);
+		cadastroCozinha.adicionar(japa);
 		
 
 	}
