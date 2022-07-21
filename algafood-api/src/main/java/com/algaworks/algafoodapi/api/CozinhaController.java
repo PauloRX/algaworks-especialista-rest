7d66a1602817766657c87a636116e6b9fd085fcf
pauloxavier <pauloxavier@192.168.0.175>
2022-07-21 18:16:17 -0300

    4.20. Manipulando a resposta HTTP com ResponseEntity
    Alterando o retorno do endpoint /cozinhas/{id} para um ResponseEntity<T>
    e especificando um HttpStatus e o body (que pode ser customizado) da
    resposta

--- algafood-api/src/main/java/com/algaworks/algafoodapi/api/CozinhaController.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/api/CozinhaController.java
@@ -3,7 +3,9 @@ package com.algaworks.algafoodapi.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +29,9 @@ public class CozinhaController {
 	public CozinhaXmlWrapper listarXml() { return new CozinhaXmlWrapper(this.listar()); }
 	
 	@GetMapping("/{cozinhaId}")
-	public Cozinha buscar(@PathVariable Long cozinhaId) { return cozinhaRepository.buscarPorId(cozinhaId); }
+	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) { 
+		Cozinha cozinha = cozinhaRepository.buscarPorId(cozinhaId); 
+		return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+	}
 	
 }
