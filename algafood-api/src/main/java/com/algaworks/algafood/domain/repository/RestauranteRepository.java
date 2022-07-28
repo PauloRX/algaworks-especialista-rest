e7fbcc9c426341fbc147a98aa4d5d0e99e5bc3ca
pauloxavier <pauloxavier@192.168.0.175>
2022-07-27 23:34:25 -0300

    5.19. Injetando o próprio repositório na implementação customizada e a anotação @Lazy
    Inclui a consulta customizada na classe RestauranteRepositoryQueries,
    implementada por RestauranteRepositoryImpl.
    Sobreescri o metodo com o trecho do codigo q estava sendo chamado
    diretamente pelo controller, afim de concentrar a regra em um unico
    local, evitndo duplicidades

--- algafood-api/src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -10,12 +10,10 @@ import org.springframework.stereotype.Repository;
 import com.algaworks.algafood.domain.model.Restaurante;
 
 @Repository
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long>, JpaSpecificationExecutor<Restaurante> {
+public interface RestauranteRepository extends JpaRepository<Restaurante, Long>, RestauranteRepositoryQueries, JpaSpecificationExecutor<Restaurante> {
 
 	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 	
 	Restaurante consultarPorNome(String nome, Long cozinhaId);
-	
-	List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal);
 	
 }
