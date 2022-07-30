973c7aafad4eb9af24c57901e31ccc9aedcdb819
pauloxavier <pauloxavier@192.168.0.175>
2022-07-29 23:00:25 -0300

    7.12. Desafio: Criando migrações e mapeando as entidades Pedido e ItemPedido

---
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Pedido.java
@@ -0,0 +1,75 @@
+package com.algaworks.algafood.domain.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
+@Entity
+public class Pedido {
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private BigDecimal subTotal;
+	
+	@Column(nullable = false)
+	private BigDecimal taxaFrete;
+	
+	@Column(nullable = false)
+	private BigDecimal valorTotal;
+	
+	@CreationTimestamp
+	@Column(nullable = false, columnDefinition = "datetime")
+	private LocalDateTime dataCriacao;
+	
+	@Column(columnDefinition = "datetime")
+	private LocalDateTime dataConfirmacao;
+	
+	@Column(columnDefinition = "datetime")
+	private LocalDateTime dataCancelamento;
+	
+	@Column(columnDefinition = "datetime")
+	private LocalDateTime dataEntrega;
+	
+	@ManyToOne
+	@JoinColumn(name = "usuario_cliente_id", nullable = false)
+	private Usuario cliente;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Restaurante restaurante;
+	
+	@Embedded
+	private Endereco enderecoEntrega;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private FormaPagamento formaPagamento;
+	
+	@OneToMany(mappedBy = "pedido")
+	private List<ItemPedido> itens = new ArrayList<>();
+	
+	private StatusPedido statusPedido;
+
+}
