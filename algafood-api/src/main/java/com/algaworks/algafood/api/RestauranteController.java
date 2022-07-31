0890222684ecba61276addd49d09f7f9a1cac793
pauloxavier <pauloxavier@192.168.0.175>
2022-07-30 23:30:24 -0300

    8.9. Desafio: usando a exception NegocioException

--- algafood-api/src/main/java/com/algaworks/algafood/api/RestauranteController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/RestauranteController.java
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
@@ -43,7 +45,11 @@ public class RestauranteController {
 
 	@PostMapping
 	public Restaurante adicionar(@RequestBody Restaurante restaurante) {
-		return cadastroRestaurante.salvar(restaurante);
+		try {
+			return cadastroRestaurante.salvar(restaurante);
+		} catch (EntidadeNaoEncontradaException e) {
+			throw new NegocioException(e.getMessage());
+		}
 	}
 
 	@PutMapping("/{restauranteId}")
@@ -55,7 +61,11 @@ public class RestauranteController {
 				"endereco", 
 				"dataCadastro",
 				"produtos");
-		return cadastroRestaurante.salvar(restauranteAtual);
+		try {
+			return cadastroRestaurante.salvar(restauranteAtual);
+		} catch (EntidadeNaoEncontradaException e) {
+			throw new NegocioException(e.getMessage());
+		}
 	}
 
 	@PatchMapping("/{restauranteId}")
