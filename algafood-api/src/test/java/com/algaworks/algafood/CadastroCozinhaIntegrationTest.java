ec549c489bf457e4490055cc0f52b145329efbcf
pauloxavier <pauloxavier@192.168.0.175>
2022-08-12 08:40:25 -0300

    10.3. Rodando um teste de integração com Spring Boot,
    JUnit e AssertJ
    
    Para esse test, utilizando jUnit5 que tem umas diferenças com o jUni4.
    Nao usei a anotacao @RunWith, pois a versao 5 faz o bind da classe teste
    com a classe, desde que esteja no pacote com o mesmo caminho.
    Para validar um unhappyPath, fiz diferente da aula, pois o @Test nao tem
    mais o atributo expected. Usei o assterThrows, passando a classe da
    exception e uma ladba com a a execucao da acao

---
+++ algafood-api/src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTest.java
@@ -0,0 +1,44 @@
+package com.algaworks.algafood;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import javax.validation.ConstraintViolationException;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.service.CadastroCozinhaService;
+
+@SpringBootTest
+public class CadastroCozinhaIntegrationTest {
+
+	@Autowired
+	CadastroCozinhaService cozinhaService;
+	
+	@Test
+	public void testarCadastroCozinhaComSuscesso() {
+		// Scenario
+		Cozinha novaCozinha = new Cozinha();
+		novaCozinha.setNome("Mexicana");
+		
+		//action
+		 novaCozinha = cozinhaService.salvar(novaCozinha);
+		
+		//validation
+		 assertThat(novaCozinha).isNotNull();
+		 assertThat(novaCozinha.getId()).isNotNull();
+	}
+	
+	@Test
+	public void testarCadastroCozinhaSemNome() {
+		assertThrows(ConstraintViolationException.class, () -> {
+			Cozinha novaCozinha = new Cozinha();
+			novaCozinha.setNome(null);
+			cozinhaService.salvar(novaCozinha);
+		});
+	}
+		
+}
---
+++ algafood-api/src/test/java/com/algaworks/algafood/CadastroRestauranteIntegrationTest.java
@@ -0,0 +1,19 @@
+package com.algaworks.algafood;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class CadastroRestauranteIntegrationTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
