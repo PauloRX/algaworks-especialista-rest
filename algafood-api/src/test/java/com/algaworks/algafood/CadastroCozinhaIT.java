f4ed631844c866fa8d7b7f1ba240932cc3e12e84
pauloxavier <pauloxavier@192.168.0.175>
2022-08-12 22:23:33 -0300

    10.7. Configurando Maven Failsafe Plugin no projeto

--- algafood-api/src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ algafood-api/src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -15,7 +15,7 @@ import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.service.CadastroCozinhaService;
 
 @SpringBootTest
-public class CadastroCozinhaIntegrationTest {
+public class CadastroCozinhaIT {
 
 	@Autowired
 	CadastroCozinhaService cozinhaService;
