3861017d757b00c8f97d0183ea62a1edceb54ccc
pauloxavier <pauloxavier@192.168.0.175>
2022-08-12 22:12:07 -0300

    10.5. Desafio: escrevendo testes de integração
    Criando um teste para Cozinha em Uso e Cozinha Inexistente

--- algafood-api/src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTest.java
+++ algafood-api/src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.service.CadastroCozinhaService;
 
@@ -38,6 +40,20 @@ public class CadastroCozinhaIntegrationTest {
 			Cozinha novaCozinha = new Cozinha();
 			novaCozinha.setNome(null);
 			cozinhaService.salvar(novaCozinha);
+		});
+	}
+	
+	@Test
+	public void deveFalhar_QuandoExcluirUmaCozinhaEmUso() {
+		assertThrows(EntidadeEmUsoException.class, () ->{
+			cozinhaService.excluir(3l);
+		});
+	}
+	
+	@Test
+	public void deveFalhar_QuandoExcluirCozinhaInexistente() {
+		assertThrows(CozinhaNaoEncontradaException.class, () ->{
+			cozinhaService.excluir(100l);
 		});
 	}
 		
