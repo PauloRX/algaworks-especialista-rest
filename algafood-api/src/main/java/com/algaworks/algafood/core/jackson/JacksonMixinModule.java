7fcdc3136d1d15549ed163140541ef5d96cc322f
pauloxavier <pauloxavier@192.168.0.175>
2022-08-14 22:54:03 -0300

    11.4. Desafio: usando @JsonIgnoreProperties e Jackson Mixin
    Usando o Mix In em Cidade e Cozinha
    Alterando a classe RestauranteMixin para abstract
    Mapeando Cidade no atributo Estado

--- algafood-api/src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
@@ -2,7 +2,11 @@ package com.algaworks.algafood.core.jackson;
 
 import org.springframework.stereotype.Component;
 
-import com.algaworks.algafood.core.mixin.RestauranteMixin;
+import com.algaworks.algafood.api.model.mixin.CidadeMixin;
+import com.algaworks.algafood.api.model.mixin.CozinhaMixin;
+import com.algaworks.algafood.api.model.mixin.RestauranteMixin;
+import com.algaworks.algafood.domain.model.Cidade;
+import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
@@ -12,7 +16,13 @@ public class JacksonMixinModule extends SimpleModule {
 	private static final long serialVersionUID = 7197240732870772798L;
 
 	public JacksonMixinModule() {
+		
 		setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
+	
+		setMixInAnnotation(Cozinha.class, CozinhaMixin.class);
+		
+		setMixInAnnotation(Cidade.class, CidadeMixin.class);
+	
 	}
 	
 }
