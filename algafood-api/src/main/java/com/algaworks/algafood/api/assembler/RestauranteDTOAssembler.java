3bba1daa937dbbd383a5c8dc5cead4bd3cccef41
pauloxavier <pauloxavier@192.168.0.175>
2022-08-15 22:52:14 -0300

    11.14. Adicionando e usando o ModelMapper
    Adicionando a lib Model Mapper para facilitar a copia da entidade para a
    representacao model

--- algafood-api/src/main/java/com/algaworks/algafood/api/assembler/RestauranteDTOAssembler.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/assembler/RestauranteDTOAssembler.java
@@ -3,28 +3,22 @@ package com.algaworks.algafood.api.assembler;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.algaworks.algafood.api.dto.CozinhaDTO;
 import com.algaworks.algafood.api.dto.RestauranteDTO;
 import com.algaworks.algafood.domain.model.Restaurante;
 
 @Component
 public class RestauranteDTOAssembler {
+	
+	@Autowired
+	ModelMapper mapper;
 
 	public RestauranteDTO toModel(Restaurante restaurante) {
 
-		CozinhaDTO cozinhaDTO = new CozinhaDTO();
-		cozinhaDTO.setId(restaurante.getCozinha().getId());
-		cozinhaDTO.setNome(restaurante.getCozinha().getNome());
-
-		RestauranteDTO restauranteDTO = new RestauranteDTO();
-		restauranteDTO.setId(restaurante.getId());
-		restauranteDTO.setNome(restaurante.getNome());
-		restauranteDTO.setTaxaFrete(restaurante.getTaxaFrete());
-		restauranteDTO.setCozinha(cozinhaDTO);
-
-		return restauranteDTO;
+		return mapper.map(restaurante, RestauranteDTO.class);
 
 	}
 
--- algafood-api/src/main/java/com/algaworks/algafood/api/assembler/RestauranteDTODesassembler.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/assembler/RestauranteDTODesassembler.java
@@ -2,26 +2,22 @@ package com.algaworks.algafood.api.assembler;
 
 import javax.validation.Valid;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.api.model.input.RestauranteInputDTO;
-import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
 
 @Component
 public class RestauranteDTODesassembler {
+	
+	@Autowired
+	ModelMapper mapper;
 
 	public Restaurante toDomainObject(@Valid RestauranteInputDTO restauranteInput) {
 
-		Cozinha cozinha = new Cozinha();
-		cozinha.setId(restauranteInput.getCozinha().getId());
-
-		Restaurante restaurante = new Restaurante();
-		restaurante.setNome(restauranteInput.getNome());
-		restaurante.setTaxaFrete(restauranteInput.getTaxaFrete());
-		restaurante.setCozinha(cozinha);
-
-		return restaurante;
+		return mapper.map(restauranteInput, Restaurante.class);
 
 	}
 	
