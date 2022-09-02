ff7a522c884b8d25f70481eefa8aabb3372ca125
pauloxavier <pauloxavier@192.168.0.175>
2022-09-02 20:40:24 -0300

    12.24. Refatorando o código de regras para transição de status
    Alterando a entidade pedido para ficar mais rica, com metodos para troca
    de status, assumindo a resposabilidade q antes estava com
    FluxoPedidoService.
    Adicionando um novo atribudo no Enum StatusPedido, para guardar status
    anteriores suportados pelo StatusPedido.
    E um metodo para verificar se nos stats anteriores contem o novoStatus.
    Se nao conter, devolve um false

--- algafood-api/src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
@@ -1,19 +1,15 @@
 package com.algaworks.algafood.domain.service;
 
-import java.time.OffsetDateTime;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.model.Pedido;
-import com.algaworks.algafood.domain.model.StatusPedido;
 
 @Service
 public class FluxoPedidoService {
 
-	private static final String NAO_PODE_ALTERAR_STATUS = "Status do pedido %d nao pode ser alterado de %s para %s";
+	
 	
 	@Autowired
 	private EmissaoPedidoService emissaoPedido;
@@ -21,49 +17,19 @@ public class FluxoPedidoService {
 	@Transactional
 	public void confirmar(Long pedidoId) {
 		Pedido pedido = emissaoPedido.buscarOuFalhar(pedidoId);
-		
-		if (!pedido.getStatus().equals(StatusPedido.CRIADO)) {
-			throw new NegocioException(String.format(NAO_PODE_ALTERAR_STATUS, 
-					pedido.getId(), 
-					pedido.getStatus().getDescricao(), 
-					StatusPedido.CONFIRMADO.getDescricao()));
-		}
-		
-		pedido.setStatus(StatusPedido.CONFIRMADO);
-		pedido.setDataConfirmacao(OffsetDateTime.now());
-		
+		pedido.confirmar();
 	}
 
 	@Transactional
 	public void cancelar(Long pedidoId) {
 		Pedido pedido = emissaoPedido.buscarOuFalhar(pedidoId);
-		
-		if (!pedido.getStatus().equals(StatusPedido.CRIADO)) {
-			throw new NegocioException(String.format(NAO_PODE_ALTERAR_STATUS,
-					pedido.getId(),
-					pedido.getStatus().getDescricao(),
-					StatusPedido.CANCELADO.getDescricao())
-			);
-		}
-		
-		pedido.setStatus(StatusPedido.CANCELADO);
-		pedido.setDataCancelamento(OffsetDateTime.now());
+		pedido.cancelar();
 	}
 
 	@Transactional
 	public void entregar(Long pedidoId) {
 		Pedido pedido = emissaoPedido.buscarOuFalhar(pedidoId);
-		
-		if (!pedido.getStatus().equals(StatusPedido.CONFIRMADO)) {
-			throw new NegocioException(String.format(NAO_PODE_ALTERAR_STATUS, 
-					pedido.getId(),
-					pedido.getStatus().getDescricao(),
-					StatusPedido.ENTREGUE.getDescricao())
-			);
-		}
-		
-		pedido.setStatus(StatusPedido.ENTREGUE);
-		pedido.setDataEntrega(OffsetDateTime.now());
+		pedido.entregar();
 	}
 	
 }
