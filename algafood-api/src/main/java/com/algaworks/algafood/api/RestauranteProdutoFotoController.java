d475f6fa834447edafe320d68caffb55fcb360f3
pauloxavier <pauloxavier@192.168.0.175>
2022-09-20 22:57:07 -0300

    4.3. Validando o tamanho máximo do arquivo
    Criando a annotation FileSize com o atributo max, para informar o
    tamanho maximo do arquivo

--- algafood-api/src/main/java/com/algaworks/algafood/api/RestauranteProdutoFotoController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/RestauranteProdutoFotoController.java
@@ -4,29 +4,32 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.UUID;
 
+import javax.validation.Valid;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
+
+import com.algaworks.algafood.api.model.input.FotoProdutoInput;
 
 @RestController
 @RequestMapping("/restaurantes/{restauranteId}/produtos/{produtoId}")
 public class RestauranteProdutoFotoController {
 	
 	@PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public void atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId, @RequestParam MultipartFile arquivo) {
-		var nomeArquivo = UUID.randomUUID() + "_" + arquivo.getOriginalFilename();
+	public void atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId, @Valid FotoProdutoInput input) {
+		var nomeArquivo = UUID.randomUUID() + "_" + input.getArquivo().getOriginalFilename();
 		var arquivoFoto = Path.of("/Users/pauloxavier/Pictures/catalogo", nomeArquivo);
 		
 		
+		System.out.println(input.getDescricao());
 		System.out.println(nomeArquivo);
-		System.out.println(arquivo.getContentType());
+		System.out.println(input.getArquivo().getContentType());
 		
 		try {
-			arquivo.transferTo(arquivoFoto);
+			input.getArquivo().transferTo(arquivoFoto);
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
